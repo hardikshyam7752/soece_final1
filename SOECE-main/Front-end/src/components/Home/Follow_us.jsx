19ac781ad776e7420d6f4f3322f48f0b9ee7a51b
Hardik Agarwal <hardikagarwal7752@gmail.com>
2026-06-21 13:12:55 +0530

    updated follow logos

--- SOECE-main/Front-end/src/components/Home/Follow_us.jsx
+++ SOECE-main/Front-end/src/components/Home/Follow_us.jsx
@@ -15,7 +15,14 @@ const Follow_us = () => {
                     
                     <div className="social_links">
                         <a href="https://instagram.com/soece_nitj" target="_blank" rel="noreferrer" className="social_btn insta">
-                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
+                            <svg
+                                width="20"
+                                height="20"
+                                viewBox="0 0 24 24"
+                                fill="#fff"
+                            >
+                                <path d="M7 2C4.239 2 2 4.239 2 7v10c0 2.761 2.239 5 5 5h10c2.761 0 5-2.239 5-5V7c0-2.761-2.239-5-5-5H7zm10.5 3a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3zM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm0 2a3 3 0 1 0 0 6 3 3 0 0 0 0-6z" />
+                            </svg>
                             <span>Instagram</span>
                         </a>
 
@@ -24,9 +31,22 @@ const Follow_us = () => {
                             <span>LinkedIn</span>
                         </a>
 
-                        <a href="https://twitter.com" target="_blank" rel="noreferrer" className="social_btn twitter" onClick={handleSoon}>
-                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path></svg>
-                            <span>X</span>
+                        <a
+                            href="https://twitter.com"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="social_btn twitter"
+                            onClick={handleSoon}
+                        >
+                            <svg
+                                width="20"
+                                height="20"
+                                viewBox="0 0 24 24"
+                                fill="#000000"
+                            >
+                                <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.64 7.584H.472l8.6-9.83L0 1.154h7.594l5.243 6.932L18.901 1.153zm-1.296 19.482h2.039L6.486 3.25H4.298l13.307 17.385z" />
+                            </svg>
+                            <span>Twitter</span>
                         </a>
                     </div>
                 </div>
